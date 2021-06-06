37fc7760068e35e65882b433fba92535d0bdbbda
hyena0608 <goatsfish@likelion.org>
2021-06-06 22:04:52 +0900

    수정중

---
+++ WebContent/jsp/commentListsort.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html" pageEncoding="utf-8" %>
+<%@ page import ="dao.*" %>
+<%
+	request.setCharacterEncoding("utf-8");
+	String which = request.getParameter("which");
+	String ascdesc = request.getParameter("ascdesc");
+	FeedDAO dao = new FeedDAO();
+	String str = dao.getCommentsort(which, ascdesc);
+	out.print(str);
+%>
--- src/main/java/dao/FeedDAO.java
+++ src/main/java/dao/FeedDAO.java
@@ -237,6 +237,31 @@ public class FeedDAO {
 		}
 	}
 	
+	public String getCommentsort(String which, String ascdesc) throws NamingException, SQLException {
+		Connection conn = ConnectionPool.get();
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			String sql = "SELECT jsonstr FROM comment ORDER BY " + which + " " + ascdesc;
+			stmt = conn.prepareStatement(sql);
+			rs = stmt.executeQuery();
+			
+			String str = "[";
+			int cnt = 0;
+			while(rs.next()) {
+				if(cnt++>0) str +=",";
+				str += rs.getString("jsonstr");
+			}
+			return str + "]";
+			
+		}finally {
+			if(rs != null) rs.close();
+			if(stmt != null) stmt.close();
+			if(conn != null) conn.close();
+		}
+	}
+
 	public String feedUserSearch(String uid) throws NamingException, SQLException {
 		Connection conn = ConnectionPool.get();
 		PreparedStatement stmt = null;
